c79844e8ac08042893716f2a93fb0219236d475c
josephidris <joseph.idris@uoit.net>
2015-03-10 10:43:13 -0400

    fixed base to laser tf

--- src/aar2_bringup/src/laserTfBroadcaster.cpp
+++ src/aar2_bringup/src/laserTfBroadcaster.cpp
@@ -2,13 +2,14 @@
 #include <tf/transform_broadcaster.h>
 #include <laser_geometry/laser_geometry.h>
 
-double theta = 0;
+double theta = -0.78539;
+int check = 1;
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "laserTfBroadcaster");
   ros::NodeHandle node;
   ros::Rate loop_rate(50);
-
+  
   static tf::TransformBroadcaster br;
   tf::Transform transform;
   transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0) );
@@ -16,7 +17,22 @@ int main(int argc, char** argv){
 
   while(ros::ok())
   {
-    theta += 0.05;
+    if (theta > 0.78539)
+    {
+	check = 0;
+    }
+    if (theta < -0.78539)
+    {
+        check = 1;
+    }
+    if (check == 1)
+    {
+    	theta += 0.05;
+    }
+    if (check == 0)
+    {
+	theta -= 0.05;
+    }
     q.setRPY(0, theta, 0);
     transform.setRotation(q);
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "laser" ));
