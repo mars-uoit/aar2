43fa6ff9d0343055046ebb38b2481ba57b7b6de5
alyychan <alyychan@gmail.com>
2015-03-21 03:16:40 -0400

    Add transform broadcast from arduino

--- src/aar2_arduino_code/LIDAR_tilting_bracket/LIDAR_tilting_bracket.ino
+++ src/aar2_arduino_code/LIDAR_tilting_bracket/LIDAR_tilting_bracket.ino
@@ -1,9 +1,27 @@
+#include <AccelStepper.h>
+#include <ArduinoHardware.h>
+#include <ros.h>
+#include <tf/transform_broadcaster.h>
+
+
 int stp = 13;  //connect pin 3 to step input
 int dir = 12;  // connect pin 2 to direction
-int a = 0;     //  gen counter
+int a = 1;     //  gen counter
+
+
+ros::NodeHandle  nh;
+
+geometry_msgs::TransformStamped t;
+tf::TransformBroadcaster broadcaster;
+
+char base_link[] = "/base_link";
+char laser[] = "/laser";
 
 void setup() 
 {                
+  nh.initNode();
+  broadcaster.init(nh);
+  
   pinMode(stp, OUTPUT);
   pinMode(dir, OUTPUT);       
 }
@@ -20,7 +38,7 @@ void loop()
     digitalWrite(stp, LOW);  
     delay(10);              
   }
-  else 
+  else if(a < 100)
   {
     digitalWrite(dir, LOW); //upwards
     a++;
@@ -28,10 +46,21 @@ void loop()
     delay(10);               
     digitalWrite(stp, LOW);  
     delay(10);
-    
-    if (a>100)    //sweep 50 in dir 2
-    {
-      a = 0;
-    }
   }
+  else if (a>100)    //sweep 50 in dir 2
+  {
+      a = 1;
+  }
+ 
+  t.header.frame_id = base_link;
+  t.child_frame_id = laser;
+  t.transform.translation.x = 1.0; 
+  t.transform.rotation.x = 0.0;
+  t.transform.rotation.y = a; 
+  t.transform.rotation.z = 0.0; 
+  t.transform.rotation.w = 1.0;  
+  t.header.stamp = nh.now();
+  broadcaster.sendTransform(t);
+  nh.spinOnce();
+  delay(10);
 }
