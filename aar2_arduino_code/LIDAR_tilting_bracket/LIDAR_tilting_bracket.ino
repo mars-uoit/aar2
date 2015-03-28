52c3be1e04ee2ea90f15cb3fd67815b5e7f3ddae
alyychan <alyychan@gmail.com>
2015-03-28 01:34:09 -0400

    Fix arduino sync issue for LIDAR stepper motor

--- aar2_arduino_code/LIDAR_tilting_bracket/LIDAR_tilting_bracket.ino
+++ aar2_arduino_code/LIDAR_tilting_bracket/LIDAR_tilting_bracket.ino
@@ -2,12 +2,10 @@
 #include <ArduinoHardware.h>
 #include <ros.h>
 #include <std_msgs/Float32.h>
-//#include <tf/transform_broadcaster.h>
-//#include <laser_geometry/laser_geometry.h>
 
 int stp = 13;  //connect pin 3 to step input
 int dir = 12;  // connect pin 2 to direction
-int a = 1;     //  gen counter
+int a = 0;     //  gen counter
 double theta = 0;  //laser angle
 
 ros::NodeHandle  nh;
@@ -15,32 +13,11 @@ ros::NodeHandle  nh;
 std_msgs::Float32 theta_msg;
 ros::Publisher pub_theta("pub_theta", &theta_msg);
 
-//geometry_msgs::TransformStamped t;
-//tf::TransformBroadcaster broadcaster;
-//char base_link[] = "/base_link";
-//char laser[] = "/laser";
-
-//ros::init(argc, argv, "laserTfBroadcaster");
-//ros::NodeHandle node;
-//ros::Rate loop_rate(50);
-//
-//static tf::TransformBroadcaster br;
-//tf::Transform transform;
-//transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0) );
-//tf::Quaternion q;
-
-
 void setup() 
 {                
   nh.initNode();
   nh.advertise(pub_theta);
-  
-//  broadcaster.init(nh);
-  
-//  node.initNode();
-//  br.init(node);
-  
-  Serial.begin(9600);
+
   pinMode(stp, OUTPUT);
   pinMode(dir, OUTPUT);       
 }
@@ -72,35 +49,11 @@ void loop()
   }
   else //restart step count
   {
-      a = 1;
+      a = 0;
   }
-  
-  Serial.println(theta);
-  
+
   theta_msg.data = theta;
   pub_theta.publish( &theta_msg );
+  
   nh.spinOnce();
- 
-//  t.header.frame_id = base_link;
-//  t.child_frame_id = laser;
-//  
-//  t.transform.translation.x = 1.0; 
-//  t.transform.rotation.x = 0.0;
-//  t.transform.rotation.y = sin(theta/2);
-//  t.transform.rotation.z = 0.0; 
-//  t.transform.rotation.w = cos(theta/2);  
-//  
-//  t.header.stamp = nh.now();
-//  broadcaster.sendTransform(t);
-//  nh.spinOnce();
-//  delay(10);
-
-
-
-
-//  q.setRPY(0, theta, 0);
-//  transform.setRotation(q);
-//  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "laser" ));
-//  ros::spinOnce();
-//  loop_rate.sleep();
 }
